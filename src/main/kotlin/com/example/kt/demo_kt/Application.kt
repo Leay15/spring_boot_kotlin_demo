f93e8cbeccfd7523e700eaf0887cc2d9a3672819
Leay15 <ivan_yael_gp@outlook.com>
2022-12-27 13:48:44 -0600

    Adds EnableSync annotation for async operations

--- src/main/kotlin/com/example/kt/demo_kt/Application.kt
+++ src/main/kotlin/com/example/kt/demo_kt/Application.kt
@@ -2,10 +2,16 @@ package com.example.kt.demo_kt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
 
 @SpringBootApplication
+@EnableAsync
 class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
+
